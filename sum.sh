f1409b3460545bac58a941ca58d8978637401052
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-04-10 00:15:00 +0300

    Update sum.sh

--- sum.sh
+++ sum.sh
@@ -3,12 +3,12 @@
 #PBS -l walltime=00:05:00
 #PBS -N Ivanov_GrandTotal
 
-#cd $PBS_O_WORKDIR
+cd $PBS_O_WORKDIR
 
 output_dir="result"
 output_file="result.txt"
 
 # Concatenate all output files and calculate sum and average
-awk '/RESULT:/ { sum += $2; count += 1 } END { printf "Total: %f\nAverage: %f\n", sum, sum/count }' ${output_dir}/solvOut_*.out >"$output_file"
+cat ./result/solvOut* | awk 'BEGIN{x=0}{x+=$0}END{print x}' >results.txt
 
 echo "Done."

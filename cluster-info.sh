8931df185124b03590a2a800497f2ed3c401fc5e
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-04-09 17:47:21 +0300

    update cluster info

--- cluster-info.sh
+++ cluster-info.sh
@@ -7,10 +7,10 @@ num_nodes=$(pbsnodes -a | grep -c '^s')
 num_cores=$(pbsnodes -a | awk '/np =/{count[$3]++} END{for (i in count) print count[i], i " cores"}')
 
 # Count the number of nodes with the same amount of memory
-num_memory=$(pbsnodes -a | awk '/physmem =/{count[$3]++} END{for (i in count) print count[i], i/1024/1024 " GB"}')
+num_memory=$(pbsnodes -a | awk '/physmem =/{count[$7]++} END{for (i in count) print count[i], i/1024/1024 " GB"}')
 
 # Calculate the average amount of memory per core
-avg_memory_per_core=$(pbsnodes -a | awk '/physmem =/{ if ($10 > 0) { total_mem+=$3; total_cores+=$10 }} END { if (total_cores > 0) { print total_mem/total_cores/1024/1024 " GB/core" } else { print "N/A" } }')
+avg_memory_per_core=$(pbsnodes -a | awk '/physmem =/{ if ($14 > 0) { total_mem+=$7; total_cores+=$14 }} END { if (total_cores > 0) { print total_mem/total_cores/1024/1024 " GB/core" } else { print "N/A" } }')
 
 # Count the number of GPUs in the cluster
 num_gpus=$(pbsnodes -a | awk '/gpus =/{count++} END{print count}')

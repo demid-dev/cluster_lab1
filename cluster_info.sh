cedcf7efe024779ad8ea89d161ccc6ff3cc474ee
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-04-09 17:54:24 +0300

    Update cluster_info.sh

--- cluster_info.sh
+++ cluster_info.sh
@@ -7,21 +7,10 @@ num_nodes=$(pbsnodes -a | grep -c '^s')
 num_cores=$(pbsnodes -a | awk '/np =/{count[$3]++} END{for (i in count) print count[i], i " cores"}')
 
 # Count the number of nodes with the same amount of memory
-num_memory=$(pbsnodes -a | awk '/physmem =/{count[$7]++} END{for (i in count) print count[i], i/1024/1024 " GB"}')
+num_memory=$(pbsnodes -a | awk '/physmem =/{count[$3/1024/1024 " GB"]++} END{for (i in count) print count[i], i}')
 
 # Calculate the average amount of memory per core
-total_mem=0
-total_cores=0
-while read -r line; do
-  mem=$(echo "$line" | awk '{print $1}')
-  cores=$(echo "$line" | awk '{print $2}')
-  if [[ $cores -gt 0 ]]; then
-    total_mem=$(echo "$total_mem + $mem" | bc)
-    total_cores=$(echo "$total_cores + $cores" | bc)
-  fi
-done <<< "$(pbsnodes -a | awk '/physmem =/ && /np =/{print $7/$14/1024/1024, $14}')"
-avg_memory_per_core=$(echo "scale=2; $total_mem/$total_cores" | bc)
-avg_memory_per_core="${avg_memory_per_core} GB/core"
+avg_memory_per_core=$(pbsnodes -a | awk '/physmem =/{ if ($0 ~ /np = ([0-9]+)/) { cores=$5; } else { cores=1; } if ($10 > 0) { print $6/cores/1024/1024 " GB/core"; } else { print "N/A"; }}' | awk '{ total+=$1; count++ } END { if (count > 0) { print total/count " GB/core"; } else { print "N/A"; } }')
 
 # Count the number of GPUs in the cluster
 num_gpus=$(pbsnodes -a | awk '/gpus =/{count++} END{print count}')

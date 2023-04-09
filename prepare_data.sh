903cfd727add9b2e3ed1e50e13677ee3bf79ece4
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-04-09 21:25:07 +0300

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "-8504 4990 30" | awk '{xmin=$1; xmax=$2; steps=$3; dx=(xmax-xmin)/steps;x=xmin; for (i=0; i<steps; i++){print x" "x+dx;x+=dx}}'
+echo "-8504 4990 30" | awk '{xmin=$1; xmax=$2; steps=$3; dx=(xmax-xmin)/steps;x=xmin; for (i=0; i<steps; i++){print x" "x+dx;x+=dx}}' >solv.inp

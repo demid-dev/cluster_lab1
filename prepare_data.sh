0f8f0e43ec931fb09ab498cd06ab876ef87c2772
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-04-09 20:37:27 +0300

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <xmin> <xmax> <steps>"
+# Parse command line arguments
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 xmin xmax steps"
     exit 1
 fi
 
+# Assign command line arguments to variables
 xmin=$1
 xmax=$2
 steps=$3
-scale=10
 
-dx=$(bc <<<"scale=$scale; ($xmax-$xmin)/$steps")
-x=$xmin
-for ((i = 0; i < $steps; i++)); do
-    x1=$(bc <<<"scale=$scale; $x+$dx")
-    echo "$x $x1"
-    x=$x1
-done >solv.inp
+# Calculate step size
+scale=10
+dx=$(echo "scale=$scale; ($xmax-$xmin)/$steps" | bc)
+
+# Generate input file with awk
+awk -v xmin="$xmin" -v dx="$dx" -v steps="$steps" \
+    'BEGIN{for(i=0;i<steps;i++){print xmin+i*dx" "xmin+(i+1)*dx}}' \
+    >solv.inp

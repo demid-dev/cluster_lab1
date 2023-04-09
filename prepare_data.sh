47f6de1e44a1eeece4a3cabc9cd5ddfe2fd4c562
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-04-09 21:00:59 +0300

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-
-if [ $# -lt 3 ]; then
-    echo "Usage: $0 xmin xmax steps"
-    exit 1
-fi
-
-xmin=$1
-xmax=$2
-steps=$3
-scale=10
-
-dx=$(echo "scale=$scale;($xmax-$xmin)/$steps" | bc)
-x=$xmin
-for i in $(seq 1 $steps); do
-    printf "%.*f %.*f\n" $scale $x $scale $(echo "scale=$scale;$x+$dx" | bc)
-    x=$(echo "scale=$scale;$x+$dx" | bc)
-done >solv.inp
+echo -8504 4990 30 | awk '{xmin=$1; xmax=$2; steps=$3; dx=(xmaxxmin)/steps;x=xmin; for (i=0; i<steps; i++){print x" "x+dx;x+=dx}}'
